392e7172237b6d99965aebd7d50cb7e48f6b0daa
Nigel <xuyi_71@qq.com>
2016-09-28 18:50:24 +0800

    新增九宫格布局
    抽出line为公共样式

---
+++ pages/list/listnavigator.js
@@ -0,0 +1,104 @@
+
+var app = getApp();
+
+Page( {
+  data: {
+    menulist1: [
+      {
+        title: '相册',
+        url: '/pages/layout/result?title=navigate1',
+        icon: '/image/icon_API_HL.png'
+      },
+      {
+        title: '收藏',
+        url: '/pages/layout/result?title=navigate1',
+        icon: '/image/icon_API_HL.png'
+      },
+      {
+        title: '卡包',
+        url: '/pages/layout/result?title=navigate1',
+        icon: '/image/icon_API_HL.png'
+      },
+      {
+        title: '相册',
+        url: '/pages/layout/result?title=navigate1',
+        icon: '/image/icon_API_HL.png'
+      },
+      {
+        title: '收藏',
+        url: '/pages/layout/result?title=navigate1',
+        icon: '/image/icon_API_HL.png'
+      },
+      {
+        title: '卡包',
+        url: '/pages/layout/result?title=navigate1',
+        icon: '/image/icon_API_HL.png'
+      },
+      {
+        title: '相册',
+        url: '/pages/layout/result?title=navigate1',
+        icon: '/image/icon_API_HL.png'
+      },
+      {
+        title: '收藏',
+        url: '/pages/layout/result?title=navigate1',
+        icon: '/image/icon_API_HL.png'
+      },
+      {
+        title: '相册',
+        url: '/pages/layout/result?title=navigate1',
+        icon: '/image/icon_API_HL.png'
+      }
+    ],
+    menulist2: [
+      {
+        title: '相册',
+        url: '/pages/layout/result?title=navigate1',
+        icon: '/image/icon_API_HL.png'
+      },
+      {
+        title: '收藏',
+        url: '/pages/layout/result?title=navigate1',
+        icon: '/image/icon_API_HL.png'
+      },
+      {
+        title: '卡包',
+        url: '/pages/layout/result?title=navigate1',
+        icon: '/image/icon_API_HL.png'
+      },
+      {
+        title: '相册',
+        url: '/pages/layout/result?title=navigate1',
+        icon: '/image/icon_API_HL.png'
+      },
+      {
+        title: '收藏',
+        url: '/pages/layout/result?title=navigate1',
+        icon: '/image/icon_API_HL.png'
+      },
+      {
+        title: '卡包',
+        url: '/pages/layout/result?title=navigate1',
+        icon: '/image/icon_API_HL.png'
+      },
+      {
+        title: '相册',
+        url: '/pages/layout/result?title=navigate1',
+        icon: '/image/icon_API_HL.png'
+      },
+      {
+        title: '收藏',
+        url: '/pages/layout/result?title=navigate1',
+        icon: '/image/icon_API_HL.png'
+      },
+      {
+        title: '相册',
+        url: '/pages/layout/result?title=navigate1',
+        icon: '/image/icon_API_HL.png'
+      }
+    ]
+  },
+  onLoad: function( options ) {
+      
+  }
+})
